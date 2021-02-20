0b3b4f907d7ba284a3762fb5a9e12885ab0a005b
Xurlz <charles.sena.sa@gmail.com>
2021-02-20 20:58:09 -0300

    Atualização das rotas em web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'App\Http\Controllers\DoadorController@index');
+
+Route::get('/doador/cadastrar', function () {
+    return view('doador/formulario');
 });
+
+Route::post('/doador/create', 'App\Http\Controllers\DoadorController@create');
+Route::get('/doador/delete', 'App\Http\Controllers\DoadorController@delete');
