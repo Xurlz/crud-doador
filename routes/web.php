35a76d7a6b5744323d02f95411717eadc9caf91d
Xurlz <charles.sena.sa@gmail.com>
2021-02-20 21:03:41 -0300

    Atualização de path em rota

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\DoadorController@index');
 
 Route::get('/doador/cadastrar', function () {
-    return view('doador/formulario');
+    return view('formulario');
 });
 
 Route::post('/doador/create', 'App\Http\Controllers\DoadorController@create');
